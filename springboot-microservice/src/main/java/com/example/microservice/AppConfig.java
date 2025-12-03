edb2bb92982b0b44eb89eda93f1d99165e548173
hevani <hayagreev.evani@dlhcorp.com>
2025-12-02 19:47:08 -0500

    added sample microservice-docker-kubernetes setup

---
+++ springboot-microservice/src/main/java/com/example/microservice/AppConfig.java
@@ -0,0 +1,8 @@
+package com.example.microservice;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+  // Add beans/configuration here if needed later
+}
