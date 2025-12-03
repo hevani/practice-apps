c2f13709b9aba687047708c61bea4fc7559775ac
hevani <hayagreev.evani@dlhcorp.com>
2025-12-02 19:49:01 -0500

    Revert "added sample microservice-docker-kubernetes setup"
    This reverts commit edb2bb92982b0b44eb89eda93f1d99165e548173.

--- springboot-microservice/src/main/java/com/example/microservice/AppConfig.java
+++
@@ -1,8 +0,0 @@
-package com.example.microservice;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class AppConfig {
-  // Add beans/configuration here if needed later
-}
